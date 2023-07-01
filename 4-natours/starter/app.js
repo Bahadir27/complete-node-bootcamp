2677e2b2a6f5f9b483d438d8e89fcbf894a50e02
Bahadir Ucer <b.ucer327@gmail.com>
2023-07-01 12:45:06 +0200

    expose errorController to app.js

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const AppError = require('./utils/appError');
-
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -23,14 +23,6 @@ app.all('*', (req, res, next) => {
   next(new AppError(`Con't find ${req.originalUrl} on the server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'Error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
