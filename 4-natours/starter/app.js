20fc5b49476751e298e492f7e78c0181aa6165d4
Bahadir Ucer <b.ucer327@gmail.com>
2023-08-16 20:22:23 +0200

    Implement Security Packages for Sanitize and Rate Limiting

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -13,15 +19,44 @@ const myLogger = function (req, res, next) {
   next();
 };
 
+// 1) GLBAL MIDDLEWARES
+// SET security HTTP headers
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour in milliseconds
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again in an hour!', // message
+});
 
+// apply the limiter to requests that begin with /api/
+app.use('/api/', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization agains NoSQL query injection
+app.use(mongoSanitize());
+// Data sanitization agains XSS
+app.use(xss());
+
+// Prevent parameter polution
+app.use(
+  hpp({
+    whitelist: ['duration'],
+  })
+);
+
+// Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(myLogger);
+
+// 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
